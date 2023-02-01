9c87c99b7c1b7543709d33c239cd5d3449105975
andreash <andreas.hogberg5@gmail.com>
2023-02-01 19:44:24 +0100

    Initial simple changes.
    Changed site title to tinniwinnies.
    Changed  navbar links.
    Started changing the layout.
    
    Signed-off-by: andreash <andreas.hogberg5@gmail.com>

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,24 +4,57 @@ import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 
 const FeatureGrid = ({ gridItems }) => (
   <div className="columns is-multiline">
-    {gridItems.map((item) => (
-      <div key={item.text} className="column is-6">
-        <section className="section">
-          <div className="has-text-centered">
-            <div
-              style={{
-                width: "240px",
-                display: "inline-block",
-              }}
-            >
-              <PreviewCompatibleImage imageInfo={item} />
-            </div>
+    {gridItems.map((item, index) => {
+      if (index % 2 === 0) {
+        return (
+          <div key={item.text} className=" feature-hero">
+            <section className="hero is-medium">
+              <div className="hero-body columns">
+
+                <div className="column">
+                  <p clasName="title">
+                    {item.title}
+                  </p>
+                  <p className="subtitle">
+                    {item.text}
+                  </p>
+                </div>
+
+                <div className="column">
+                  <PreviewCompatibleImage imageInfo={item} />
+                </div>
+
+              </div>
+            </section>
           </div>
-          <p>{item.text}</p>
-        </section>
-      </div>
-    ))}
-  </div>
+        );
+
+      } else {
+        return (
+          <div key={item.text} className=" feature-hero">
+            <section className="hero is-medium">
+              <div className="hero-body columns">
+
+                <div className="column">
+                  <PreviewCompatibleImage imageInfo={item} />
+                </div>
+
+                <div className="column">
+                  <p clasName="title">
+                    {item.title}
+                  </p>
+                  <p className="subtitle">
+                    {item.text}
+                  </p>
+                </div>
+
+              </div>
+            </section>
+          </div>
+        );
+      }
+    })}
+  </div >
 );
 
 FeatureGrid.propTypes = {
@@ -29,6 +62,7 @@ FeatureGrid.propTypes = {
     PropTypes.shape({
       image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
       text: PropTypes.string,
+      title: PropTypes.string,
     })
   ),
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,5 @@
 import React from "react";
 import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
-import logo from "../img/logo.svg";
-
 const Navbar = class extends React.Component {
   constructor(props) {
     super(props);
@@ -23,11 +20,11 @@ const Navbar = class extends React.Component {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: "is-active",
-            })
+            navBarActiveClass: "is-active",
+          })
           : this.setState({
-              navBarActiveClass: "",
-            });
+            navBarActiveClass: "",
+          });
       }
     );
   }
@@ -42,7 +39,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+              <h1 className="is-size-3	has-text-weight-bold	">TinnieWinnies</h1>
             </Link>
             {/* Hamburger menu */}
             <div
@@ -62,35 +59,30 @@ const Navbar = class extends React.Component {
             id="navMenu"
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
-            <div className="navbar-start has-text-centered">
+            <div className="navbar-end has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                Om kenneln
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Valpar
+              </Link>
+              <Link className="navbar-item" to="/products">
+                Våra hundar
+              </Link>
+              <Link className="navbar-item" to="/products">
+                Dagbok
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                Bilder
+              </Link>
+              <Link className="navbar-item" to="/products">
+                Hundar vi minns
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+                Kontakt
               </Link>
             </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
+
           </div>
         </div>
       </nav>
